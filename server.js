822a07e4e42a4bd8a6b215fa5f006613e45965dd
Julie Mathews <juliem.forever@gmail.com>
2019-04-21 21:37:43 -0700

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var request = require("request");
 var Note = require("./models/note");
 var Article = require("./models/article");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/scrapeHW";
+var MONGODB_URI = process.env.MONGODB_URI || "ongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
